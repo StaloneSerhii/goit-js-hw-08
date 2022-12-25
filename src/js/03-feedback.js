5152d93085d756c5cbf76cfbf984e7fb3c86de56
StaloneSerhii <sergio.fly4405@gmail.com>
2022-12-25 19:53:22 +0200

    L;.,

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,37 @@
 import throttle from 'lodash.throttle';
 
-const formInput = {
-    form:document.querySelector('.feedback-form'),
-    textarea:document.querySelector('.feedback-form textarea')
-};
+const STORAGE_KEY = 'feedback-form-state';
+const objAll = localStorage.getItem(STORAGE_KEY);
+const parsDate = JSON.parse(objAll)
 
+const forms = document.querySelector('form');
+
+forms.addEventListener('input', throttle(onForm, 500));
+forms.addEventListener('submit', onClickSubj);
+saveDate()
+
+function onForm (e) {
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ email: forms.elements.email.value, message: forms.elements.message.value }));
+
+    parsDate[e.target.name] = e.target.value;
+
+}
+
+
+function saveDate() {
+  
+    forms.elements.email.value = parsDate.email
+    forms.elements.message.value = parsDate.message
+}
+
+
+function onClickSubj(e) {
+    console.log(parsDate);
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+ 
     
-
-formInput.form.addEventListener('submit', onForm);
-formInput.textarea.addEventListener('input', onTextarea);
-
-function onTextarea (evt) {
-    const message = evt.currentTarget.value;
-
-    console.log(evt);
+       console.log(objAll);
 }
